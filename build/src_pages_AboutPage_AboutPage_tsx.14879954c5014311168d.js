7764f5c5e4d8ee6aaa9b27066b668935efcd5fb9
OliverVK98 <v1155533@gmail.com>
2023-03-24 00:03:28 -0400

    initial project structure

--- build/src_pages_AboutPage_AboutPage_tsx.14879954c5014311168d.js
+++ build/src_pages_AboutPage_AboutPage_tsx.14879954c5014311168d.js
@@ -13,7 +13,7 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony export */ });
 /* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-runtime */ "./node_modules/react/jsx-runtime.js");
 
-var AboutPage = function () {
+const AboutPage = () => {
     return ((0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx)("div", { children: "ABOUT PAGE" }));
 };
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (AboutPage);
@@ -22,4 +22,4 @@ var AboutPage = function () {
 /***/ })
 
 }]);
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX0Fib3V0UGFnZV9BYm91dFBhZ2VfdHN4LmUwMTNhZmViZGVkNzk5ZjY0ODVkLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2hEO0FBQ0EsWUFBWSxzREFBSSxVQUFVLHdCQUF3QjtBQUNsRDtBQUNBLGlFQUFlLFNBQVMsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3N0dWR5LWFwcC8uL3NyYy9wYWdlcy9BYm91dFBhZ2UvQWJvdXRQYWdlLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBqc3ggYXMgX2pzeCB9IGZyb20gXCJyZWFjdC9qc3gtcnVudGltZVwiO1xudmFyIEFib3V0UGFnZSA9IGZ1bmN0aW9uICgpIHtcbiAgICByZXR1cm4gKF9qc3goXCJkaXZcIiwgeyBjaGlsZHJlbjogXCJBQk9VVCBQQUdFXCIgfSkpO1xufTtcbmV4cG9ydCBkZWZhdWx0IEFib3V0UGFnZTtcbiJdLCJuYW1lcyI6W10sInNvdXJjZVJvb3QiOiIifQ==
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoic3JjX3BhZ2VzX0Fib3V0UGFnZV9BYm91dFBhZ2VfdHN4LjE0ODc5OTU0YzUwMTQzMTExNjhkLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7Ozs7Ozs7O0FBQWdEO0FBQ2hEO0FBQ0EsWUFBWSxzREFBSSxVQUFVLHdCQUF3QjtBQUNsRDtBQUNBLGlFQUFlLFNBQVMsRUFBQyIsInNvdXJjZXMiOlsid2VicGFjazovL3N0dWR5LWFwcC8uL3NyYy9wYWdlcy9BYm91dFBhZ2UvQWJvdXRQYWdlLnRzeCJdLCJzb3VyY2VzQ29udGVudCI6WyJpbXBvcnQgeyBqc3ggYXMgX2pzeCB9IGZyb20gXCJyZWFjdC9qc3gtcnVudGltZVwiO1xuY29uc3QgQWJvdXRQYWdlID0gKCkgPT4ge1xuICAgIHJldHVybiAoX2pzeChcImRpdlwiLCB7IGNoaWxkcmVuOiBcIkFCT1VUIFBBR0VcIiB9KSk7XG59O1xuZXhwb3J0IGRlZmF1bHQgQWJvdXRQYWdlO1xuIl0sIm5hbWVzIjpbXSwic291cmNlUm9vdCI6IiJ9
