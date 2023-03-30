d5fc8ec64e3dea6f6eb92ea326668105f92d2b82
OliverVK98 <v1155533@gmail.com>
2023-03-30 00:07:05 -0400

    Added stories for components

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,19 +1,32 @@
 import React from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink';
+import { useTranslation } from 'react-i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
     className?:string
 }
 
-const Navbar = ({ className }: NavbarProps) => (
-    <div className={classNames(cls.Navbar, {}, [className])}>
-        <div className={cls.links}>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>Home Page</AppLink>
-            <AppLink theme={AppLinkTheme.SECONDARY} to="/about">About Page</AppLink>
+const Navbar = ({ className }: NavbarProps) => {
+    const { t } = useTranslation();
+
+    return (
+        <div className={classNames(cls.Navbar, {}, [className])}>
+            <div className={cls.links}>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
+                    {
+                        t('Home Page')
+                    }
+                </AppLink>
+                <AppLink theme={AppLinkTheme.SECONDARY} to="/about">
+                    {
+                        t('About Page')
+                    }
+                </AppLink>
+            </div>
         </div>
-    </div>
-);
+    );
+};
 
 export default Navbar;
